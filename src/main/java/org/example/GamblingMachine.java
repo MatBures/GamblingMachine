c8f35adbe2dc650e35c410405e1ca2e9262f99f5
Dendramoth <vitsi@seznam.cz>
2022-12-17 14:25:58 +0100

    code review implementation

--- src/main/java/org/example/GamblingMachine.java
+++ src/main/java/org/example/GamblingMachine.java
@@ -12,62 +12,80 @@ Good Luck! (sadly you cant win anything :D, but it works!)
 
 public class GamblingMachine {
 
-    //Setting variables, imports and lists.
+    private static final int MAX_VALUE = 100;
+    private static final int MIN_VALUE = 1;
+    private static final int NUMBER_OF_TIPS = 3;
+    private static final int TOTAL_AMOUNT_OF_NUMBERS_IN_GAME = 50;
+
     public static void main(String[] args) {
+
+        // Let user input his 3 numbers
+        List<Integer> threeLotteryNumbers = getPlayerTips();
+        System.out.println("Your lucky numbers are :" + threeLotteryNumbers);
+
+        // Generate random numbers for the game
+        List<Integer> listOfRandomGeneratedNumbers = generateRandomNumbersForGame();
+
+        // Find out how many player tips were correct.
+        listOfRandomGeneratedNumbers.retainAll(threeLotteryNumbers);
+        System.out.println("Matched numbers in lottery are: " + listOfRandomGeneratedNumbers);
+
+        //Print result for the player.
+        printGameResult(listOfRandomGeneratedNumbers.size());
+    }
+
+    private static List<Integer> getPlayerTips() {
+        List<Integer> threeLotteryNumbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int lotteryNumber;
-        List<Integer> threeLotteryNumbers = new ArrayList<Integer>();
-        List<Integer> listOfRandomGeneratedNumbers = new ArrayList<Integer>();
-        int max = 100;
 
         System.out.print("Hello, please enter three numbers from 1 - 100 range to try win lottery jackpot!");
 
         //While loop that scans your 3 inputted numbers from 1-100 and adding them to list of threeLotteryNumbers
         do {
-            lotteryNumber = scanner.nextInt();
-            if (lotteryNumber > 100) {
+            int lotteryNumber = scanner.nextInt();
+            if (lotteryNumber > MAX_VALUE) {
                 System.out.println("You inputted higher number than 100, try again.");
 
-            } else if (lotteryNumber < 1) {
+            } else if (lotteryNumber < MIN_VALUE) {
                 System.out.println("You inputted lower number than 1, try again.");
 
             } else {
                 threeLotteryNumbers.add(lotteryNumber);
             }
-        } while (threeLotteryNumbers.size() < 3);
+        } while (threeLotteryNumbers.size() < NUMBER_OF_TIPS);
 
-        System.out.println("Your lucky numbers are :" + threeLotteryNumbers);
+        return threeLotteryNumbers;
+    }
 
-            //While loop that generates 50 random numbers and adds them to listOfRandomGeneratedNumbers, every single number is unique (they cant repeat).
-            do {
-                int randomGeneratedNumber = random.nextInt(max) + 1;
-                boolean sameNumberSearch = listOfRandomGeneratedNumbers.contains(randomGeneratedNumber);
-                if (sameNumberSearch) {
+    private static List<Integer> generateRandomNumbersForGame() {
+        Random random = new Random();
+        List<Integer> listOfRandomGeneratedNumbers = new ArrayList<>();
 
-                } else {
-                    listOfRandomGeneratedNumbers.add(randomGeneratedNumber);
-                }
+        //While loop that generates 50 random numbers and adds them to listOfRandomGeneratedNumbers, every single number is unique (they cant repeat).
+        do {
+            int randomGeneratedNumber = random.nextInt(MAX_VALUE) + MIN_VALUE;
+            boolean numberIsAlreadyInList = listOfRandomGeneratedNumbers.contains(randomGeneratedNumber);
+            if (!numberIsAlreadyInList) {
+                listOfRandomGeneratedNumbers.add(randomGeneratedNumber);
+            }
 
-            } while (listOfRandomGeneratedNumbers.size() < 50);
+        } while (listOfRandomGeneratedNumbers.size() < TOTAL_AMOUNT_OF_NUMBERS_IN_GAME);
 
-            //Special command retainAll is matching the numbers of randoms and user's inputted numbers to one list = listOfRandomGeneratedNumbers
-            listOfRandomGeneratedNumbers.retainAll(threeLotteryNumbers);
-            System.out.println("Matched numbers in lottery are: " + listOfRandomGeneratedNumbers);
+        return listOfRandomGeneratedNumbers;
+    }
 
-
+    private static void printGameResult(int numberOfCorrectGuesses) {
         //Checking by Ifs the size of list to write correct system.out by matched numbers in lists.
-        if(listOfRandomGeneratedNumbers.size() == 0)
-        {
+        if (numberOfCorrectGuesses == 0) {
             System.out.println("Sorry you lost everything, try again!");
         }
-        if (listOfRandomGeneratedNumbers.size()==1) {
+        if (numberOfCorrectGuesses == 1) {
             System.out.println("Only 1 number was correct, try again!");
         }
-        if (listOfRandomGeneratedNumbers.size()==2) {
+        if (numberOfCorrectGuesses == 2) {
             System.out.println("2 numbers were correct, almost there, try again!");
         }
-        if (listOfRandomGeneratedNumbers.size()==3) {
+        if (numberOfCorrectGuesses == 3) {
             System.out.println("CONGRATULATIONS! YOU WON!");
         }
     }
